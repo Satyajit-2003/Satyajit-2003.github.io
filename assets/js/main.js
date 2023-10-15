a28e095f9abe9ab28ede11e693aa2a38927b7185
Satyajit Satapathy <satyajit1470@gmail.com>
2023-10-15 19:17:50 +0530

    Added a project and modified main.js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -379,7 +379,7 @@ function loadProjects() {
       var rowDiv = null;
       var keys = Object.keys(data);
 
-      for (var i = 0; i < keys.length; i++) {
+      for (var i = keys.length - 1; i >= 0; i--) {
         var project = data[keys[i]];
         var title = project.title;
         var category = project.category;
